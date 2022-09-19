ef5c1de30765deb4d728d6ad6af586a75253b9d7
Ossama Qureshi <ossamasq1@outlook.com>
2022-09-19 19:50:43 -0400

    Third Commit (Randomized Pastel Colored Hearts)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import HeartsList from './components/HeartList';
-import Heart from './components/Heart'
 import Header from './components/Header'
 import './App.css';
 
-const message = 'cool cud';
+
 
 function App() {
     return (
       <>
         <Header />
-        <HeartsList msg={message} />
+        <HeartsList />
       </>
     );
-}
+};
 
 export default App;
